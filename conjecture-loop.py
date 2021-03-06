ca4200f7224b1abe59b2ee88fb186a4e781954f6
Venkata Rama Krishna Kambhampati <61433274+cyberinfy@users.noreply.github.com>
2021-03-06 11:54:51 +0530

    Add files via upload

---
+++ conjecture-loop.py
@@ -0,0 +1,19 @@
+from time import sleep
+def coundrum(n):
+    t=n
+    iteration = 0
+    while True:
+        iteration += 1
+        if t%2 == 0:
+            t /= 2
+        else:
+            t *= 3
+            t += 1
+        if iteration%3 == 0:
+            print(t)
+        else:
+            print(t, end=", ")
+        sleep(.4)
+
+n = int(input("Please enter a number of your choice: "))
+coundrum(n)
---
+++ conjecture.py
@@ -0,0 +1,25 @@
+from time import sleep
+import sys
+
+def coundrum(n):
+    print(n, end="-> ")
+    t=n
+    iteration = 0
+    while True:
+        iteration += 1
+        if t%2 == 0:
+            t /= 2
+        else:
+            t *= 3
+            t += 1
+        if iteration<9 or t<4:
+            print(t, end=", ")
+        elif t==4:
+            print("....,",t,end=", ")
+        if t==1:
+            break
+    print()
+    sleep(.1)
+
+for i in range(1,sys.maxsize):
+    coundrum(i)
